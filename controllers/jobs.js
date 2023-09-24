6d0d9f055a111a4a77181615edd7e9825f8fc4dd
Ahmed <aahmedsamy.as@gmail.com>
2023-09-24 10:50:40 +0300

    create jobs model

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,26 @@
-const getAllJobs = async (req, res)=>{
-    res.send("get all jobs")
+const Job = require('../models/Job')
+const statusCodes = require('http-status-codes')
+const getAllJobs = async (req, res) => {
+    const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+    res.status(statusCodes.OK).json({jobs, count: jobs.length})
 }
 
-const getJob = async (req, res)=>{
+const getJob = async (req, res) => {
     res.send("get job")
 }
 
-const createJob = async (req, res)=>{
-    res.send("create job")
+const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(statusCodes.CREATED).json({job})
 }
 
-const updateJob = async (req, res)=>{
+const updateJob = async (req, res) => {
     res.send("update job")
 }
 
 
-const deleteJob = async (req, res)=>{
+const deleteJob = async (req, res) => {
     res.send("delete job")
 }
 
--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide email'],
         match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})$/,
             'Please provide a valid email',
         ],
         unique: true,
