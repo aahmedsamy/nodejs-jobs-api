5eacd0df0390f00f7ac7e38cd748299b1e84d3c4
Ahmed <aahmedsamy.as@gmail.com>
2023-09-24 15:48:49 +0300

    added extra security layers

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
+// EXTRA SECURITY PACKAGES
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -14,8 +20,17 @@ const jobsRouter = require('./routes/jobs')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 mins
+  max: 100 // limit each IP to 100 requests per windowMs
+}))
 
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 // extra packages
 
 // routes
